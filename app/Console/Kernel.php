4a28c1c6ddd91f45a26a118bbd2137dedc24d096
caijuntao <caijuntao@lxbird.com>
2022-09-22 15:49:59 +0800

    更新

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use App\Http\Controllers\h2ddd\AutoExChange;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
