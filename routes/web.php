deb56dcc5dfb4616fb087ab64958995adda30ed0
caijuntao <caijuntao@lxbird.com>
2022-09-23 15:50:19 +0800

    增加微信公众号配置

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::any('h2ddd/exChangeSteps', [App\Http\Controllers\h2ddd\UserController::class, 'exChangeSteps2']);
+Route::any('wechat', [App\Http\Controllers\Wechat\WechatReplyController::class, 'reply']);
