4b073ab38f6dc5999fc056c456a1c84962befc84
caijuntao <caijuntao@lxbird.com>
2022-09-26 00:24:44 +0800

    更新八蛇兑换为ip代理方式

--- app/Http/Controllers/h2ddd/UserController.php
+++ app/Http/Controllers/h2ddd/UserController.php
@@ -76,17 +76,24 @@ class UserController extends Controller
             return response()->json(['code' => 201, 'msg' => '缺少必填参数或参数不对', 'data' => $validate->errors()->toArray()]);
         }
         try {
-
             $steps = $request_data['steps'];
             $phone = $request_data['phone'];
             $password = $request_data['password'];
-            list($result_code,$result_msg,$result_data) = $this->login($phone,$password);
+            list($result_code,$result_msg,$result_data) = $this->testLogin($phone,$password);
             if($result_code == true && $result_data['token']){
+                $record_iplist = self::getProxyIp();
+                $ip = $record_iplist['ip'];
+                $http_port = $record_iplist['http_port'];
+                $s5_port = $record_iplist['s5_port'];
+                $expire_at_timestamp = $record_iplist['expire_at'];
                 $url = "https://www.h2ddd.com/api/steps/exchange?steps=".$steps;
                 $client = new GuzzleHttp\Client;
                 $response = $client->request('GET', $url, [
                     'headers' => ['token' => $result_data['token']],
-                    'verify' => false
+                    'verify' => false,
+                    'proxy' => [
+                        'http'  => $ip.':'.$http_port, // Use this proxy with "http"
+                    ]
                 ]);
                 $result = json_decode( $response->getBody(), true);
                 Log::info('phone:'.$phone.'已进行兑换:'.json_encode($result));
@@ -148,58 +155,93 @@ class UserController extends Controller
             return array(false,'访问八蛇服务器失败!',array('token'=>null));
         }
     }
-    public function testLogin(Request $request)
+    public function testLogin($phone,$password)
     {
-        $record_iplist = self::getProxyIp();
-        $ip = $record_iplist['ip'];
-        $http_port = $record_iplist['http_port'];
-        $s5_port = $record_iplist['s5_port'];
-        $expire_at_timestamp = $record_iplist['expire_at'];
-        // 要访问的目标页面
-        $targetUrl = "https://www.h2ddd.com/api/login/login?phone=13691775300&password=Zhanghui@1";
+        $param = [
+            'phone'     => $phone,
+            'password'  => $password,
+        ];
+        $record_user = UserModel::where($param)->first();
+        if(!empty($record_user) && !empty($record_user->token) && $record_user->expired_date >= time()){
+            return array(true,'get success',array('token'=>$record_user->token));
+        }
+        try {
+            $record_iplist = self::getProxyIp();
+            $ip = $record_iplist['ip'];
+            $http_port = $record_iplist['http_port'];
+            $s5_port = $record_iplist['s5_port'];
+            $expire_at_timestamp = $record_iplist['expire_at'];
+            // 要访问的目标页面
+            $targetUrl = "https://www.h2ddd.com/api/login/login?phone=13691775300&password=Zhanghui@1";
 
-        // 代理服务器
-        $proxyServer = $ip.":".$s5_port;
+            // 代理服务器
+            $proxyServer = $ip.":".$s5_port;
 
-        // 隧道身份信息
-        $ch = curl_init();
+            // 隧道身份信息
+            $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, $targetUrl);
+            curl_setopt($ch, CURLOPT_URL, $targetUrl);
 
-        curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, false);
+            curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, false);
 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-        // 设置代理服务器
-        // curl_setopt($ch, CURLOPT_PROXYTYPE, 0); //http
+            // 设置代理服务器
+            // curl_setopt($ch, CURLOPT_PROXYTYPE, 0); //http
 
-        curl_setopt($ch, CURLOPT_PROXYTYPE, 5); //sock5
+            curl_setopt($ch, CURLOPT_PROXYTYPE, 5); //sock5
 
-        curl_setopt($ch, CURLOPT_PROXY, $proxyServer);
+            curl_setopt($ch, CURLOPT_PROXY, $proxyServer);
 
-        // 设置隧道验证信息
-        curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_BASIC);
+            // 设置隧道验证信息
+            curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_BASIC);
 
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 60; Windows NT 5.1; SV1; .NET CLR 2.0.50727;)");
+            curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 60; Windows NT 5.1; SV1; .NET CLR 2.0.50727;)");
 
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
 
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 
-        curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_HEADER, false);
 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_FAILONERROR, false);
 
-        $result = curl_exec($ch);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        curl_close($ch);
+            $result = curl_exec($ch);
 
-        var_dump($result);
+            curl_close($ch);
+            Log::info('请求登录返回结果:'.$result);
+            $response = json_decode($result,true);
+            if($response['code'] == 1){
+                //success
+                $update_data = [
+                    'password'      => $password,
+                    'token'         => $response['data']['app_token'],
+                    'updated_at'    => time(),
+                    'expired_date'  => strtotime('+10 minutes'),
+                ];
+                if(UserModel::where('phone',$phone)->first()){
+                    $success = UserModel::where('phone',$phone)->update($update_data);
+                    return array(true,'update success',array('token'=>$update_data['token']));
+                }else{
+                    $update_data['phone']       = $phone;
+                    $update_data['created_at']  = time();
+                    $success = UserModel::insert($update_data);
+                    return array(true,'create success',array('token'=>$update_data['token']));
+                }
+            }else{
+                return array(false,$response['msg'],array('token'=>null));
+            }
+        }catch (\Exception $exception){
+            Log::info('访问八蛇服务器失败:'.$exception->getMessage());
+            return array(false,'访问八蛇服务器失败!',array('token'=>null));
+        }
     }
     public static function getProxyIp(){
-        $return_data = DB::table('h2ddd_iplist')->where('expire_at','<',time())->first();
+        $return_data = DB::table('h2ddd_iplist')->where('expire_at','>',time())->first();
         if(!empty($return_data)){
-            return $return_data->toArray();
+            return json_decode(json_encode($return_data),true);
         }
         $host = "http://chiyunapi.market.alicloudapi.com";
         $path = "/proxy/shared/get";
@@ -209,7 +251,7 @@ class UserController extends Controller
         array_push($headers, "Authorization:APPCODE " . $appcode);
         array_push($headers, "Content-Type:application/json");
         array_push($headers, "Accept:application/json");
-        $querys = "amount=1&expire=5-30&format=json&splitter=rn&proxy_type=http&white_ip=120.78.184.135";
+        $querys = "amount=1&expire=30&format=json&splitter=rn&proxy_type=http&white_ip=120.78.184.135,0.0.0.0";
         $bodys = "";
         $url = $host . $path . "?" . $querys;
 
@@ -239,5 +281,24 @@ class UserController extends Controller
             return $insert_data;
         }
         return false;
+    }
+    private function getClientIp()
+    {
+        if (getenv('HTTP_CLIENT_IP')) {
+            $ip = getenv('HTTP_CLIENT_IP');
+        }
+        if (getenv('HTTP_X_REAL_IP')) {
+            $ip = getenv('HTTP_X_REAL_IP');
+        } elseif (getenv('HTTP_X_FORWARDED_FOR')) {
+            $ip = getenv('HTTP_X_FORWARDED_FOR');
+            $ips = explode(',', $ip);
+            $ip = $ips[0];
+        } elseif (getenv('REMOTE_ADDR')) {
+            $ip = getenv('REMOTE_ADDR');
+        } else {
+            $ip = '0.0.0.0';
+        }
+
+        return $ip;
     }
 }
