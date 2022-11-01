d07ea3454349e0ee9c8e9b747b01778a98c8f08f
caijuntao <caijuntao@lxbird.com>
2022-11-01 17:22:38 +0800

    增加登录日志

--- app/Http/Controllers/Administrator/UsersController.php
+++ app/Http/Controllers/Administrator/UsersController.php
@@ -9,6 +9,7 @@ use App\Models\Users;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use JWTAuth;
+use Log;
 
 class UsersController extends Controller
 {
@@ -29,6 +30,7 @@ class UsersController extends Controller
             ->first();
         if(!empty($user)){
             if(!Hash::check($password,$user->password)){
+                Log::info('用户:'.$username.'登录失败:密码不匹配');
                 return response()->json([
                     'code'  => 201,
                     'msg'   => '密码不匹配',
@@ -36,6 +38,7 @@ class UsersController extends Controller
                 ]);
             }
         }else{
+            Log::info('用户:'.$username.'登录失败:查找不到用户');
             return response()->json([
                 'code'  => 201,
                 'msg'   => '查找不到用户',
@@ -49,8 +52,15 @@ class UsersController extends Controller
         //方式三、下面这种方式必须使用密码登录
 //        $token = auth('api')->attempt($user->toArray());
         if(!$token ){
-            return response()->json(['error' => 'Unauthorized'],401);
+            Log::info('用户:'.$username.'登录失败:token生成失败');
+            return response()->json([
+                'code'  => 201,
+                'msg'   => 'token生成失败',
+                'data'  => null
+            ]);
+//            return response()->json(['error' => 'Unauthorized'],401);
         }
+        Log::info('用户:'.$username.'登录成功:'.$token);
         return $this->respondWithToken($token,$user);
     }
     protected function respondWithToken($token, $data)
