eb6794efb4f50211e38a2c0ccc15bf1fa324a2e4
caijuntao <caijuntao@lxbird.com>
2023-02-15 16:02:20 +0800

    测试

--- app/Service/AliossService.php
+++ app/Service/AliossService.php
@@ -102,7 +102,7 @@ class AliossService
             'file' => [], // 真正的文件数组
             'dir'  => [], // 真正的目录数组
         ];
-        Log::info('开始查找');
+        Log::info('开始查找1111');
         while (true) {
             $options = [
                 'delimiter' => $delimiter,
